157983dfe9d4754c923e5a6b35499f34f1755374
Iamneo <techadmin@iamneo.ai>
2023-08-04 13:52:40 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/services/Taskservice.java
@@ -0,0 +1,62 @@
+package com.examly.springapp.services;
+import com.examly.springapp.entity.Task;
+import java.util.*;
+public interface TaskService {
+    public List<Task> getallTask();
+    public Task getTask(long taskid);
+    public Task addTask(Task t);
+    public Task updateTask(Task t);
+    public void deleteTask(long id);
+}
+
+
+taskserviceImpl
+
+
+package com.examly.springapp.services;
+import java.util.List;
+
+
+import com.examly.springapp.dao.Taskdao;
+import com.examly.springapp.entity.Task;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TaskServiceImpl implements TaskService {
+    @Autowired
+    private Taskdao td;
+    public List<Task> getallTask()
+    {
+        
+        return td.findAll();
+    }
+    public Task getTask(long thn)
+    {
+        
+        return td.getOne(thn);
+    }
+    @Override
+    public Task addTask(Task t) {
+       return td.save(t);
+        
+    }
+    @Override
+    public Task updateTask(Task t) {
+       
+        return td.save(t);
+
+
+    }
+    @Override
+    public void deleteTask(long id) {
+        
+        Task entity=td.getOne(id);
+        td.delete(entity);
+    }
+
+
+}
